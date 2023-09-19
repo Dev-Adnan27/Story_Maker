2008d6fa6c3f39409ec8e3bf1221a1a889417f3c
Adnan Hasnain <123970469+Dev-Adnan27@users.noreply.github.com>
2023-09-19 18:56:23 +0500

    Project Completed and Is Live Too

---
+++ app.js
@@ -0,0 +1,87 @@
+const stories = [
+    {
+        title: "A Day at the Beach",
+        storyFirstHalf: "You wake up early in the morning, excited about the day ahead. The sun is shining brightly, and the weather is perfect for a trip to the beach with your best friend, ${friend}. You quickly pack your beach bags and head out to pick up ${friend}. As you arrive at her house, ${friend} greets you with a big smile. You can tell that she's just as excited as you are for this day out. You both hop into your car and start the journey to the beach. Once you reach the beach, you lay out your beach towels, set up an umbrella for some shade, and apply sunscreen to protect yourselves from the sun's rays. The sound of the waves crashing against the shore is soothing, and you and ${friend} waste no time in running towards the water. You spend hours playing in the waves, building sandcastles, and collecting seashells together. ${friend}'s laughter is infectious, and you can't help but feel grateful for such an amazing friend.",
+        storySecondhalf: "As the day goes on, you have a picnic by the shore, enjoying sandwiches and fruits you packed earlier. You share stories, dreams, and laughter with ${friend}, making this day even more special. The sun begins to set, casting a beautiful orange and pink hue across the sky. You and ${friend} sit together, watching the sunset, feeling content and appreciative of each other's company. As the day comes to a close, you both pack up your things and head back to the car, tired but with hearts full of wonderful memories. It was truly a perfect day at the beach with your best friend, ${friend}."
+    },
+    {
+        title: "An Adventure in the Park",
+        storyFirstHalf: "One bright and sunny morning, you decide to embark on an exciting adventure with your best friend, ${friend}. The destination: the sprawling city park, where endless possibilities await. You meet up with ${friend} at the park's entrance, and you can see the excitement in his eyes. With a backpack filled with snacks and drinks, you're ready to explore the wonders of the park together. Your adventure begins with a hike through the lush forest trails. The tall trees provide shade, and the birds serenade you as you walk. You and ${friend} come across a picturesque clearing with a bubbling stream, and you decide to take a break there, dipping your feet in the cool water and enjoying a snack. Next, you both decide to rent a paddleboat and take it out on the park's tranquil lake. As you paddle together, you share stories, dreams, and even sing your favorite songs. It's a moment of pure joy and camaraderie.",
+        storySecondhalf: "The park has a wildlife reserve, and you're lucky to spot some deer grazing by the edge of the woods. ${friend} takes out his camera and captures the beauty of nature, preserving the memory of this adventure. Feeling a bit adventurous, you and ${friend} decide to climb a hill in the park, which offers a breathtaking view of the entire city. At the top, you sit and take in the vista, feeling on top of the world with your best friend by your side. As the day turns into evening, you find a cozy spot for a picnic dinner. The sun sets in a blaze of colors, and you and ${friend} savor the delicious food and each other's company. Finally, you both head back to the park's entrance, tired but exhilarated by the day's adventure. It was an unforgettable day in the park with your best friend, ${friend}, filled with laughter, exploration, and the joy of shared experiences."
+    },
+    {
+        title: "Exploring a Hidden Waterfall",
+        storyFirstHalf: "You and your best friend, ${friend}, have heard rumors of a hidden waterfall deep in the forest. Today, you decide to embark on an exciting journey to find it. Armed with backpacks and a sense of adventure, you start hiking through the dense forest. The path is rugged, but the anticipation of discovering the hidden gem keeps you going. Along the way, you and ${friend} encounter colorful birds and exotic flowers. The sounds of nature surround you, creating a sense of tranquility and wonder.",
+        storySecondhalf: "After hours of hiking, you finally hear the sound of rushing water. Excitement fills the air as you and ${friend} follow the sound to find a breathtaking waterfall hidden among the trees. The cascading water sparkles in the sunlight, and you both stand in awe of its beauty. You spend the day swimming in the crystal-clear pool at the base of the waterfall, feeling the cool mist on your skin. It's a day of discovery and wonder, and you couldn't have asked for a better adventure with your best friend, ${friend}."
+    },
+    {
+        title: "A Cozy Winter Cabin Getaway",
+        storyFirstHalf: "Winter has arrived, and you and your best friend, ${friend}, decide to escape to a cozy cabin in the mountains. As you drive through the snowy landscape, you can't help but admire the serene beauty of the winter wonderland. Upon reaching the cabin, you're greeted by a warm fireplace and a view of snow-covered trees. The crackling fire creates a sense of warmth and coziness, and you and ${friend} unpack your bags and settle in for a relaxing weekend getaway.",
+        storySecondhalf: "You and ${friend} spend the day building a snowman, sipping hot cocoa by the fire, and sharing stories of past winter adventures. In the evening, you both bundle up and stargaze from the cabin's porch, marveling at the clear night sky. The twinkling stars create a sense of wonder and serenity. It's a perfect winter getaway with your best friend, ${friend}, filled with laughter and the magic of the season."
+    },
+    {
+        title: "A Road Trip to Remember",
+        storyFirstHalf: "You and your best friend, ${friend}, decide to embark on an epic road trip across the country. With a map in hand and your favorite tunes playing on the radio, you hit the open road. The journey is filled with scenic drives, quirky roadside attractions, and spontaneous detours. Each mile brings new adventures and discoveries, and you and ${friend} can't help but feel a sense of freedom and excitement.",
+        storySecondhalf: "Along the way, you encounter friendly locals, try regional cuisine, and share unforgettable moments. Whether it's watching the sunrise over a desert landscape or camping under the starry night sky, every day of the road trip brings new adventures and strengthens the bond between you and your best friend, ${friend}. The open road is a canvas of endless possibilities, and you and ${friend} are painting a masterpiece of memories."
+    },
+    {
+        title: "A Magical Day at an Amusement Park",
+        storyFirstHalf: "You and your best friend, ${friend}, decide to spend a day at a magical amusement park. As you enter the park, you're greeted by the sight of colorful rides, delicious treats, and an atmosphere filled with laughter and excitement. You can hardly contain your enthusiasm as you explore the park together. The scent of cotton candy",
+        storySecondhalf: " and popcorn fills the air, and the sound of joyful screams from roller coasters creates a sense of exhilaration. You and ${friend} go on thrilling roller coasters, play carnival games, and indulge in cotton candy and ice cream. The day is a whirlwind of fun and adventure, and you both end it with a spectacular fireworks show that lights up the night sky. The dazzling display of colors and the sound of fireworks bursting overhead create a sense of magic and wonder. It's a day you'll cherish forever with your best friend, ${friend}."
+    },
+    {
+        title: "A Spontaneous Camping Trip",
+        storyFirstHalf: "One weekend, you and your best friend, ${friend}, decide to embark on a spontaneous camping trip. With a backpack filled with camping gear and a sense of adventure, you head into the wilderness. The two of you set up camp by a tranquil river, surrounded by the beauty of nature. The rustling leaves and chirping birds create a symphony of natural sounds, soothing your souls.",
+        storySecondhalf: "You spend the day hiking, fishing, and exploring the wilderness. As night falls, you build a campfire and share stories under the starry sky. The crackling of the fire and the sounds of the forest create a sense of peace and connection between you and your best friend, ${friend}. The Milky Way stretches across the sky, and you both lay on your backs, marveling at the beauty of the universe. It's a camping trip filled with spontaneity and unforgettable moments, a true escape from the hustle and bustle of daily life."
+    },
+    {
+        title: "A Day of Volunteering Together",
+        storyFirstHalf: "You and your best friend, ${friend}, decide to spend a day volunteering together. You choose a local charity and dedicate your time to a meaningful cause. Whether it's helping at a food bank, assisting with animal rescue, or participating in a community cleanup, you both give your all to make a positive impact. The sense of purpose and the smiles on people's faces create a profound sense of fulfillment.",
+        storySecondhalf: "Throughout the day, you witness the power of teamwork and the joy of helping others. The smiles on people's faces and the gratitude you receive fill your hearts with warmth. As the day comes to an end, you and ${friend} reflect on the importance of giving back and vow to continue making a difference together. The experience of making the world a better place is a bond that strengthens your friendship even further, and you look forward to more volunteering adventures with your best friend, ${friend}."
+    },
+    {
+        title: "A Romantic Getaway to a Mountain Retreat",
+        storyFirstHalf: "With your best friend, ${friend}, you embark on a romantic getaway to a secluded mountain retreat. The cozy cabin is nestled among tall pine trees, and a gentle snowfall creates a picturesque winter scene. Inside, a roaring fireplace warms the room, and the soft glow of candlelight sets a romantic ambiance. You and ${friend} share a bottle of wine and a delicious meal by the fire, reminiscing about your cherished friendship and the adventures you've shared.",
+        storySecondhalf: "After a night of laughter and heartfelt conversation, you wake up to a stunning sunrise over the snow-covered mountains. You and ${friend} bundle up and go for a walk, hand in hand, through the pristine snow. The air is crisp and refreshing, and the world feels peaceful and still. You return to the cabin for a cozy breakfast by the fire, savoring the simple joy of being together. It's a romantic getaway that strengthens your bond and creates unforgettable memories with your best friend, ${friend}."
+    },
+    {
+        title: "A Day of Creativity and Art",
+        storyFirstHalf: "You and your best friend, ${friend}, decide to spend a day exploring your creative sides. You visit an art studio where you can both try your hand at painting. Surrounded by canvases, brushes, and vibrant colors, you and ${friend} let your imaginations run wild. You create your own unique works of art, each reflecting your individual styles and personalities. It's a day filled with laughter, experimentation, and the joy of self-expression.",
+        storySecondhalf: "After your artistic adventure, you and ${friend} visit a local gallery to admire the work of other artists and gain inspiration. The gallery is a treasure trove of creativity, and you discuss your favorite pieces and what they mean to you. In the evening, you return home with your newfound masterpieces and hang them proudly in your living spaces, a reminder of the special day you shared with your best friend, ${friend}."
+    },
+    {
+        title: "A Culinary Adventure in the City",
+        storyFirstHalf: "You and your best friend, ${friend}, are self-proclaimed foodies, and today is a day of culinary exploration in the city. You start with a visit to a bustling farmers' market, where you sample artisanal cheeses, fresh-baked bread, and exotic fruits. The vibrant colors and delicious aromas make your mouths water, and you can't resist trying a bit of everything.",
+        storySecondhalf: "After the market, you and ${friend} embark on a culinary tour of the city, visiting food trucks, cafes, and renowned restaurants. Each stop offers a new gastronomic delight, from gourmet tacos to decadent desserts. You savor every bite and share your culinary discoveries with each other, making mental notes of the dishes you want to recreate at home. It's a day of indulgence and gastronomic adventure with your best friend, ${friend}."
+    },
+    {
+        title: "A Relaxing Spa Day",
+        storyFirstHalf: "You and your best friend, ${friend}, are in desperate need of relaxation, so you decide to treat yourselves to a day at the spa. The moment you step into the tranquil spa, you're enveloped in a sense of calm and serenity. You both opt for a full day of pampering, starting with massages that melt away your stress and tension. The soothing music and aromatic oils transport you to a state of pure bliss.",
+        storySecondhalf: "After your massages, you enjoy a soothing soak in hot mineral baths, followed by facials and body treatments that leave your skin glowing. In between treatments, you and ${friend} lounge by the pool, sipping herbal tea and catching up on life. The spa day is a much-needed escape from the demands of everyday life, and you both leave feeling refreshed and rejuvenated, promising to make relaxation a regular part of your friendship."
+    }
+];
+
+friendName.addEventListener('blur', () => {
+    const friend = friendName.value;
+    if (friend !== "") {
+        let i = Math.floor(Math.random() * stories.length);
+        const story = stories[i];
+        title.innerHTML = story.title;
+        story_halfS.innerHTML = story.storyFirstHalf.replace(/\${friend}/g, friend);
+        story_halfE.innerHTML = story.storySecondhalf.replace(/\${friend}/g, friend);
+    } else {
+        alert("Enter Your Best Friend's Name");
+    }
+});
+let readAloud = new SpeechSynthesisUtterance();
+
+storyReader.addEventListener('click', () => {
+    let fullStory = title.innerHTML + story_halfS.innerHTML + story_halfE.innerHTML;
+    readAloud.text = fullStory;
+    window.speechSynthesis.speak(readAloud);
+    window.speechSynthesis.onerror = function(event) {
+        alert('Speech Synthesis Error:', event.error);
+    };
+});
+
+
